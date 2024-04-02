559bcec34616799b4777d404a9f2f919142e5140
abkavi9924 <165765669+abkavi9924@users.noreply.github.com>
2024-04-02 16:19:57 +0530

    Update animations.js

--- animations.js
+++ animations.js
@@ -1,21 +1,17 @@
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    let slides = document.getElementsByClassName("slide");
-    
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    slideIndex++;
-
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-
-    slides[slideIndex - 1].style.display = "block";
-    
-    setTimeout(showSlides, 5000);
+ import "./styles.css";
+ let index = 0;
+displayImages();
+function displayImages() {
+  let i;
+  const images = document.getElementsByClassName("image");
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+  index++;
+  if (index > images.length) {
+    index = 1;
+  }
+  images[index-1].style.display = "block";
+  setTimeout(displayImages, 2000); 
 }
 
