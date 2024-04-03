c46f057cc890537b8e9eefc2d12b0c61f49cf01e
abkavi9924 <165765669+abkavi9924@users.noreply.github.com>
2024-04-04 01:11:37 +0530

    Update animations.js

--- animations.js
+++ animations.js
@@ -1,17 +1,25 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  const slides = document.getElementsByClassName("slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 3000); // Change slide every 3 seconds (3000 milliseconds)
+}
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
+document.querySelector('.prev').addEventListener('click', function() {
+  plusSlides(-1);
+});
 
-function showSlides(n) {
-  let i;
-  const slides = document.getElementsByClassName("slides");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  slides[slideIndex-1].style.display = "block";  
-}
+document.querySelector('.next').addEventListener('click', function() {
+  plusSlides(1);
+});
