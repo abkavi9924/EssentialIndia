da393a0e5dab8d9133ec59ae592e2e18869205a3
abkavi9924 <165765669+abkavi9924@users.noreply.github.com>
2024-04-04 01:20:10 +0530

    Automation of slideshow

--- animations.js
+++ animations.js
@@ -5,11 +5,13 @@ function showSlides() {
   let i;
   const slides = document.getElementsByClassName("slides");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
+    slides[i].classList.remove("active");  
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}    
   slides[slideIndex-1].style.display = "block";  
+  slides[slideIndex-1].classList.add("active");
   setTimeout(showSlides, 3000); // Change slide every 3 seconds (3000 milliseconds)
 }
 
