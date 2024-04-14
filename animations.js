aa3f44f6596877e36792039fbc23830a9c7630c8
abkavi9924 <165765669+abkavi9924@users.noreply.github.com>
2024-04-14 17:58:07 +0530

    animation

--- animations.js
+++ animations.js
@@ -1,24 +1,3 @@
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let i;
-  const slides = document.getElementsByClassName("slides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-    slides[i].classList.remove("active");  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  slides[slideIndex-1].style.display = "block";  
-  slides[slideIndex-1].classList.add("active");
-  setTimeout(showSlides, 3000); // Change slide every 3 seconds (3000 milliseconds)
-}
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
 document.addEventListener("DOMContentLoaded", function() {
   const texts = document.querySelectorAll('p, h1,h2,h3');
 
