ad63b8cd81c7248d85a05de9451e97499339c331
abkavi9924 <165765669+abkavi9924@users.noreply.github.com>
2024-04-15 17:38:37 +0530

    update

--- general.js
+++ general.js
@@ -1,12 +1,23 @@
 // Add active class on click
 document.querySelectorAll('nav ul li a').forEach(item => {
-    item.addEventListener('click', function() {
+  item.addEventListener('click', function() {
       // Remove active class from all other links
       document.querySelectorAll('nav ul li a').forEach(link => {
-        link.classList.remove('active');
+          link.classList.remove('active');
       });
       // Add active class to the clicked link
       this.classList.add('active');
-    });
   });
-  
+});
+
+// Add active class to the menu item corresponding to the current page
+window.addEventListener('DOMContentLoaded', function() {
+  // Get the current page URL
+  const currentPageURL = window.location.href;
+  // Find the corresponding menu item and add the active class
+  document.querySelectorAll('nav ul li a').forEach(link => {
+      if (link.href === currentPageURL) {
+          link.classList.add('active');
+      }
+  });
+});
